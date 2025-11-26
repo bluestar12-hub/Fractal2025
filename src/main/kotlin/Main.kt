39cb176a1d7bb800e194b5e8bd0564838b900b81
macbook <serbakovd187@gmail.com>
2025-11-26 19:51:10 +0300

    добавил функцию 5: Возведение в степень

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.window.application
 import app.ui.PaintPanel
 import app.ui.SelectionPanel
 import app.viewmodels.MainViewModel
+import kotlin.math.pow
 
 @Composable
 @Preview
@@ -43,6 +44,9 @@ fun main(): Unit = application {
         App()
     }
     student2Print()
+    student1Print()
+    student3Print()
+    student5Print()
 }
 
 // пробный коммит
@@ -58,3 +62,25 @@ private fun student2Print() {
     println("Student 2: $x * $y = $product")
 }
 
+
+private fun student1Print() {
+    val a = 5
+    val b = 3
+    val sum = a + b
+    println("Student 1: $a + $b = $sum")
+}
+
+private fun student3Print() {
+    val a = 15
+    val b = 4
+    val difference = a - b
+    println("Student 3: $a - $b = $difference")
+}
+
+private fun student5Print() {
+    val base = 2
+    val exponent = 5
+    val power = base.toDouble().pow(exponent.toDouble()).toInt()
+    println("Student 5: $base^$exponent = $power")
+//привет привет :)
+}
