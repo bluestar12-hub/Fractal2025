46344eecad63d8b8a66443f0f68cee1b0528a8e1
stepa_aaa <pivnoykaban@gmail.com>
2025-12-10 13:35:58 +0300

    Дописал тесты для FractalFunctions.kt, Tour. Добавил иконку для приложения

--- src/test/kotlin/app/viewmodels/MainViewModelExportTest.kt
+++ src/test/kotlin/app/viewmodels/MainViewModelExportTest.kt
@@ -10,8 +10,6 @@ class MainViewModelExportTest {
     fun `saveAsJpg does not throw exception`() {
         val vm = MainViewModel()
 
-        // Этот тест просто проверяет, что метод не падает с исключением
-        // Фактический экспорт файла будет происходить в UI-потоке
         assertDoesNotThrow {
             vm.saveAsJpg()
         }
